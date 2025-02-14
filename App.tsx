94c1e23f0d17b1e00a3dd0d67b8b426dc2bff2e2
Dsneezy <dkawesomer@gmail.com>
2025-02-13 17:21:06 -0800

    Initial commit

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from "twrnc";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text style={styles.title}>Weather</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -13,8 +14,20 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
+    paddingTop: 50
   },
-});
+  title: {
+    fontSize: 70,
+    fontWeight: 'bold',
+    color: '#FFF',
+    textAlign: 'center',
+    borderColor: '#58b55a',
+    borderWidth: 15,
+    backgroundColor: '#58b55a'
+  }
+})
+
+
